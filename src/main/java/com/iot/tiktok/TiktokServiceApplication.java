07378edfd9626b8f5599ac01d690146731d8e48e
drayee <1473443474@qq.com>
2026-01-25 00:13:09 +0800

    start: 建立简单的 tiktok 的项目框架

---
+++ src/main/java/com/iot/tiktok/TiktokServiceApplication.java
@@ -0,0 +1,13 @@
+package com.iot.tiktok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiktokServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiktokServiceApplication.class, args);
+    }
+
+}
