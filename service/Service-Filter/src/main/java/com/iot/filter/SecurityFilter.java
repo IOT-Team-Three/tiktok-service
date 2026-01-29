701cdc8d75538f28a228bc276926db51c68c938f
drayee <1473443474@qq.com>
2026-01-29 22:27:42 +0800

    temp-fix:将 spring security 拦截器默认让所有 rust 通过以便调试

--- service/Service-Filter/src/main/java/com/iot/filter/SecurityFilter.java
+++ service/Service-Filter/src/main/java/com/iot/filter/SecurityFilter.java
@@ -1,4 +1,22 @@
 package com.iot.filter;
 
-public class SecurityFilter {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityFilter extends AbstractHttpConfigurer<SecurityFilter, HttpSecurity> {
+
+    @Bean
+    public DefaultSecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/api/**").permitAll()
+                .anyRequest().authenticated()
+        );
+        return http.build();
+    }
 }
